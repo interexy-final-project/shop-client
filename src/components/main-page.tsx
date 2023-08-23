5e1d54f0b55f586e44e1ad5a6b37f5b91e4dcc48
Irina Dubrovskaya <irina.dubrovskaya95@gmail.com>
2023-08-23 18:37:14 +0300

    feat: added categories to main page

--- src/components/main-page.tsx
+++ src/components/main-page.tsx
@@ -1,13 +1,25 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 import CommonHeader from "../app/components/common-header";
 import HeroImage from "../assets/imgs/shop-hero.png";
 import Footer from "../app/components/footer";
 import { colors } from "../assets/themes";
 import HeroLeaves from "../assets/imgs/hero-leaves.png";
 import HeroShopnow from "../assets/imgs/hero-shopnow.png";
+import { ITypeCardProps, TypeCard } from "../app/components/type-card";
 
 export const MainPage: React.FC = () => {
+  const [types, setTypes] = useState<ITypeCardProps[]>([]);
+
   return (
     <>
       <CommonHeader />
@@ -52,7 +64,7 @@ export const MainPage: React.FC = () => {
           </Stack>
         </Box>
 
-        <Box margin="0 6.25rem">
+        <Box margin="0 6.25rem" position="relative">
           <Stack direction="row" justifyContent="center" position="relative">
             <img src={HeroLeaves} />
             <img src={HeroShopnow} />
@@ -64,18 +76,53 @@ export const MainPage: React.FC = () => {
               display: "flex",
               justifyContent: "center",
               alignItems: "flex-start",
-              p: "8rem 12rem",
+              p: "4rem 12rem ",
+              position: "absolute",
+              top: "0%",
+              left: "5%",
+              maxWidth: "50rem",
             }}
-            position="absolute"
           >
-            <Typography variant="h1">
+            <Typography variant="h2" color={colors.white}>
               WE MADE YOUR EVERYDAY FASHION BETTER!
             </Typography>
-            <Typography variant="h4">
+            <Typography variant="h6" color={colors.white}>
               In our journey to improve everyday fashion, euphoria presents
               EVERYDAY wear range - Comfortable & Affordable fashion 24/7
             </Typography>
             <Button variant="shop-white-button">Shop now</Button>
+          </Stack>
+        </Box>
+
+        <Box>
+          <Stack>
+            <Typography variant="h3" color={colors.secondary} sx={{}}>
+              Categories For Men
+            </Typography>
+            {types.map((type) => (
+              <Grid item key={type.id}>
+                <TypeCard
+                  id={type.id}
+                  image={type.image}
+                  typeName={type.typeName}
+                />
+              </Grid>
+            ))}
+          </Stack>
+
+          <Stack>
+            <Typography variant="h3" color={colors.secondary}>
+              Categories For Women
+            </Typography>
+            {types.map((type) => (
+              <Grid item key={type.id}>
+                <TypeCard
+                  id={type.id}
+                  image={type.image}
+                  typeName={type.typeName}
+                />
+              </Grid>
+            ))}
           </Stack>
         </Box>
       </Box>
