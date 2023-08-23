d7a5800be80b672871b0d657b4c626f98fbb0aff
Irina Dubrovskaya <irina.dubrovskaya95@gmail.com>
2023-08-23 13:41:08 +0300

    feat: added main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@emotion/react";
 import { theme } from "./assets/themes";
 import AppRoutes from "./app.routers";
 import CategoryPage from "./app/category/category.page";
 import ProductDetails from "./app/product/product-details.page";
 function App(): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <CssBaseline />
-        <AppRoutes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <CssBaseline />
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
